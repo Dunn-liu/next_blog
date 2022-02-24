779922036d8a802d3567cbec214bb1e85df821cd
DunnLiu <15817294245@163.com>
2022-02-24 17:20:36 +0800

    fix：修改基地址

--- src/utils/pushUrlToBaidu.js
+++ src/utils/pushUrlToBaidu.js
@@ -2,14 +2,14 @@ const axios = require('axios')
 const http = require('http')
 async function push() {
   // 获取文章id
-  const res = await axios.get('https://api.codespring.top/blog/queryArticle', { params: { pageSize: 100000 } })
+  const res = await axios.get('https://api.liuqidong.com/blog/queryArticle', { params: { pageSize: 100000 } })
   const { data } = res
   if (data && data.code === 200) {
     data.data.map(async item => {
-      const content = `https://blog.codespring.top/article?id=${item.id}`
+      const content = `https://blog.liuqidong.com/article?id=${item.id}`
       const options = {
         host: "data.zz.baidu.com",
-        path: 'http://data.zz.baidu.com/urls?site=https://blog.codespring.top&token=TFVTB3On7AQeMNGX',//接口的调用地址
+        path: 'http://data.zz.baidu.com/urls?site=https://blog.liuqidong.com&token=TFVTB3On7AQeMNGX',//接口的调用地址
         method: "post",
         "User-Agent": "curl/7.12.1",
         headers: {
